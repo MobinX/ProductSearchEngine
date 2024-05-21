5c9d0ac2e4b1e42e0064ccd3e4a86712c71e8fa2
Mobin Chowdhury <47663360+MobinX@users.noreply.github.com>
2024-05-21 19:45:44 +0000

    test

--- pipeline/daraz/crawlProducts.py
+++ pipeline/daraz/crawlProducts.py
@@ -2,7 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import json
+from xata.client import XataClient
 
+xata = XataClient(api_key="xau_vhTUq5SIpC2R5u7ua6zDHPKjQhkpGT9e2",db_url="https://Mobin-Chowdhury-s-workspace-eh41hn.us-east-1.xata.sh/db/productSearch:main")
+
+def uploadProductLinks(table_name, productLink):
+    table_schema = {
+        "columns": [
+            {
+                "name": "link",
+                "type": "string",
+            },
+        ]
+    }
+    try:
+        
+        assert xata.table().create(table_name).is_success()
+        resp = xata.table().set_schema(table_name, table_schema)
+        assert resp.is_success(), resp
+         
+        record = {
+            "link": productLink
+         }
+        respi = xata.records().insert(table_name, record)
+        assert respi.is_success(), respi
+    except AssertionError as error:
+        
+        record = {
+            "link": productLink
+         }
+        respi = xata.records().insert(table_name, record)
+        assert respi.is_success(), respi
+        
+    
+# Open the browser
 options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
@@ -29,15 +62,19 @@ with open('../../store/daraz-categories.json') as file:
         totalProducts = totalProducts.replace(" items found for", "")
         totalProducts = totalProducts.replace(",", "")
         totalProducts = int(totalProducts)
+        print(f"Total products: {totalProducts}")
         products = driver.find_elements(By.ID, "id-a-link")
         productPagesCount = int(totalProducts / len(products)) + 1
+        print(f"Total pages: {productPagesCount}")
         productLinks = []
+        
         for i in range(1, productPagesCount):
             print(f"Processing page {i} of category {category}")
             driver.get(f'{url}?page={i}')
             products = driver.find_elements(By.ID, "id-a-link")
             for product in products:
                 productLinks.append(product.get_attribute("href"))
+                uploadProductLinks(category, product.get_attribute("href"))
                 print(product.get_attribute("href"))
         result[category] = productLinks
 
@@ -45,28 +82,4 @@ with open('../../store/daraz-categories.json') as file:
 # Write the result to a JSON file
 with open('../../store/daraz-products.json', 'w') as file:
     json.dump(result, file)
-# Process the data
-# ...
 
-# Example: Print the contents of the JSON file
-# print(data)
-
-# print("running")
-# driver.get('https://www.daraz.com.bd/smartphones/')
-# totalProducts = driver.find_element(By.XPATH, "//div[@class=' tips--QRnmZ']//span[1]").text
-# totalProducts = totalProducts.replace(" items found for", "")
-# totalProducts = int(totalProducts)
-# print(totalProducts)
-
-# products = driver.find_elements(By.ID, "id-a-link")
-# productPagesCount = int(totalProducts / len(products)) + 1
-# print(productPagesCount)
-# driver.quit()
-
-# for i in range(1, productPagesCount):
-#     driver = webdriver.Chrome(options=options)
-#     driver.get(f'https://www.daraz.com.bd/smartphones/?page={i}')
-#     products = driver.find_elements(By.ID, "id-a-link")
-#     for product in products:
-#         print(product.get_attribute("href"))
-#     driver.quit()
